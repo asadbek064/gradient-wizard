8537ac4426dbc9a807e5876d411a4db30ac86c7e
Asadbek Karimov <asadbekasadbek@gmail.com>
2023-11-05 23:15:37 -0800

    Update gtm.js

--- src/gtm.js
+++ src/gtm.js
@@ -5,4 +5,4 @@ w[l] = w[l] || []; w[l].push({
 }); var f = d.getElementsByTagName(s)[0],
     j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
     'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-NN5FG6H');
+})(window, document, 'script', 'dataLayer', '#');
